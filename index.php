244eef81fa13deeddd31bc471716c0d41c8ffc3a
Alexis <Alexis.perront@hotmail.fr>
2020-03-18 17:09:55 +0100

    ajout de c'est fichier

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "alexis" =>[
+        "city" => "Voves",
+        "weapon" => "pistolet lazer"
+    ]
+];
+var_dump($characters["alexis"]);
+
+
